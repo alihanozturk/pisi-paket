1a28fbf36f10e6a2f25c2bc4561847cda17baec8
alihanozturk <alihan.ozturk@fenerbahce.com.tr>
2013-11-25 23:55:44 +0200

    düzeltme

--- qelectrotech/actions.py
+++ qelectrotech/actions.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import qt4
-#from pisi.actionsapi import pisitools
-
-# if pisi can't find source directory, see /var/pisi/qelectrotech/work/ and:
-# WorkDir="qelectrotech-"+ get.srcVERSION() +"/sub_project_dir/"
+from pisi.actionsapi import get
 
 def setup():
     qt4.configure()
@@ -17,20 +15,3 @@ def build():
 
 def install():
     qt4.install()
-
-# Take a look at the source folder for these file as documentation.
-#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
-
-# If there is no install rule for a runnable binary, you can 
-# install it to binary directory.
-#    pisitools.dobin("qelectrotech")
-
-# You can use these as variables, they will replace GUI values before build.
-# Package Name : qelectrotech
-# Version : 0.3
-# Summary : QElectroTech kendi elektrik şemalarına oluşturmanıza yardımcı olmak için tasarlanmış bir uygulamadır.
-
-# For more information, you can look at the Actions API
-# from the Help menu and toolbar.
-
-# By PiSiDo 2.0.0
