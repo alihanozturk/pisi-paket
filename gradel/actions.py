13d7afc2bd8bb9c54b12f1161160e77fe64a1467
alihanozturk <alihan.ozturk@fenerbahce.com.tr>
2013-12-21 22:35:35 +0200

    delete

--- gradel/actions.py
+++ gradel/actions.py
@@ -4,13 +4,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="gradel-0.12.1"
-
 def install():
+    shelltools.export("HOME",get.workDIR())
+    WorkDir="gradel-0.12.1"
     shelltools.system("./compile.sh")
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     shelltools.system("./install.sh")
